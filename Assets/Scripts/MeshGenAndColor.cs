f77510f39f904449794eee23bae2848e79bd4b72
Florian Glökler <florian.gloekler@online.de>
2019-05-22 19:43:51 +0200

    Added script, which generates a mesh (terrain) and color the vertices based on their height with a gradient (colormap). Also added a shader, which passes through the calculated colors from the script and applies a lambert shading to the terrain. (first two points from exercise 3)

---
+++ Assets/Scripts/MeshGenAndColor.cs
@@ -0,0 +1,129 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+public class MeshGenAndColor : MonoBehaviour
+{
+    Mesh mesh;
+    // vertices and triangles array for mesh generation
+    Vector3[] vertices;
+    int[] triangles;
+
+    // --- public variables are editable in the Unity Inspector afterwards
+    // x and z size of generating mesh
+    public int xSize = 20;
+    public int zSize = 20;
+    // height scale of the generating mesh
+    public float heightScale = 3f;
+    // gradient (colormap) of for the terrain
+    public Gradient gradient;
+
+    Color[] colors;
+    float minTerrainHeight;
+    float maxTerrainHeight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // add new mesh object to the mesh filter defined on the empty game object
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        // on every frame, create the shape and update the mesh
+        CreateShape();
+        UpdateMesh();
+    }
+
+    void CreateShape()
+    {
+        // define vertex count 
+        // need one more vertex on each coordinate than the defined mesh size
+        // (e.g. need 3*3 vertices for a plane out of 4 squares)
+        vertices = new Vector3[(xSize + 1) * (zSize + 1)];
+
+        // loop over all vertices to assign each position
+        for (int i = 0, z = 0; z <= zSize; z++)
+        {
+            for (int x = 0; x <= xSize; x++)
+            {
+                // calculate height of each vertex with the perlin noise function 
+                // and multiply it with the height scale set by the user
+                float y = Mathf.PerlinNoise(x * .3f, z * .3f) * heightScale;
+
+                // assign the vertex position with x, y and z coordinate
+                vertices[i] = new Vector3(x, y, z);
+
+                // set the maximum and minimum terrain height variables for the
+                // colorizing of the terrain afterwards over a gradient
+                if (y > maxTerrainHeight)
+                    maxTerrainHeight = y;
+
+                if (y < minTerrainHeight)
+                    minTerrainHeight = y;
+
+                i++;
+            }
+        }
+
+        // define count of triangle points (6 for one mesh quad)
+        triangles = new int[xSize * zSize * 6];
+
+        // variables to track actual vertex and actual triangle
+        int vert = 0;
+        int tris = 0;
+
+        // loop over all mesh quads and define the triangle points
+        for (int z = 0; z < zSize; z++)
+        {
+            for (int x = 0; x < xSize; x++)
+            {
+                // first triangle (bottom left, top left, bottom right of a quad)
+                triangles[tris + 0] = vert + 0;
+                triangles[tris + 1] = vert + xSize + 1;
+                triangles[tris + 2] = vert + 1;
+                // second triangle (bottom right, top left, top right of a quad)
+                triangles[tris + 3] = vert + 1;
+                triangles[tris + 4] = vert + xSize + 1;
+                triangles[tris + 5] = vert + xSize + 2;
+
+                vert++;
+                tris += 6;
+            }
+            vert++;
+        }
+
+        // define color array (same length tan vertices array, because each vertex has one color)
+        colors = new Color[vertices.Length];
+
+        // loop over the vertices and set the colors (same array index in the vertices and color array) 
+        for (int i = 0, z = 0; z <= zSize; z++)
+        {
+            for (int x = 0; x <= xSize; x++)
+            {
+                // get height value as percentage of the minimum and maximum height of the total mesh
+                float height = Mathf.InverseLerp(minTerrainHeight, maxTerrainHeight, vertices[i].y);
+                // calculate and set color at the calculated "time" (height value as percentage value) of the gradient/colormap 
+                colors[i] = gradient.Evaluate(height);
+                //Debug.Log("height: " + height + " color: " + colors[i]);
+                i++;
+            }
+        }
+    }
+
+    void UpdateMesh()
+    {
+        mesh.Clear();
+
+        // set all calculated or defined mesh properties
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.colors = colors;
+
+        mesh.RecalculateNormals();
+    }
+}
---
+++ Assets/Shaders/ColorTerrainShader.shader
@@ -0,0 +1,88 @@
+﻿Shader "Unlit/ColorTerrainShader"
+{
+    Properties
+    {
+        // Reflectance of the ambient light
+		_Ka("Ambient Reflectance", Range(0, 1)) = 0.5
+
+		// Reflectance of the diffuse light
+		_Kd("Diffuse Reflectance", Range(0, 1)) = 0.5
+    }
+    SubShader
+    {
+        Pass
+        {
+            // indicate that our pass is the "base" pass in forward
+            // rendering pipeline. It gets ambient and main directional
+            // light data set up; light direction in _WorldSpaceLightPos0
+            // and color in _LightColor0
+            Tags {"LightMode"="ForwardBase"}
+
+            CGPROGRAM
+            // definition of used shaders and their names
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+            #include "Lighting.cginc" // for lightning
+
+            // input struct of vertex shader
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float4 color : COLOR;
+                float3 normal : NORMAL;
+            };
+
+            // struct for data from vertex to fragment shader
+            struct v2f
+            {
+                // vertex positions in homogeneous coordinates
+                float4 pos : SV_POSITION;
+                // base vertice color
+                float4 color : COLOR0;
+                // ambient light color
+                float4 amb : COLOR1;
+                // diffuse light color
+                float4 diff : COLOR2;
+            };
+
+            float _Ka, _Kd;
+
+            // VERTEX SHADER
+            v2f vert (appdata vertexIn)
+            {
+                v2f vertexOut;
+                // transform vertices from object coordinates to clip coordinates
+                vertexOut.pos = UnityObjectToClipPos(vertexIn.vertex);
+
+                // transform normal vectors to world coordinates
+                half3 worldNormal = UnityObjectToWorldNormal(vertexIn.normal);
+
+                // calculate ambient light color
+                vertexOut.amb = float4(ShadeSH9(half4(worldNormal,1)),1);
+
+                // calculate diffuse light color
+                half nl = max(0, dot(worldNormal, _WorldSpaceLightPos0.xyz));
+                vertexOut.diff = nl * _LightColor0;
+                
+                // pass through the vertice color
+                vertexOut.color = vertexIn.color;
+                return vertexOut;
+            }
+            
+            // FRAGMENT SHADER
+            float4 frag (v2f fragIn) : SV_Target {
+                // set base color
+                float4 color = fragIn.color;
+
+                // multiply base color with ambient and diffuse light
+                color *= (_Ka * fragIn.amb + _Kd * fragIn.diff);
+
+                return color;
+            }
+
+            ENDCG
+        }
+    }
+}
