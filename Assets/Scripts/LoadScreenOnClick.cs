fe3be0ff47ab5fff395ee7ef0773f26774d1eeff
Matthias Hecht <matthiashecht98@outlook.com>
2019-06-21 16:05:06 +0200

    Added an Menu

---
+++ Assets/Scripts/LoadScreenOnClick.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadScreenOnClick : MonoBehaviour
+{
+    public InputField TerrainOffsetInput;
+    public InputField GaussianVarianceInput;
+
+    public void clicked(int sceneIndex)
+    {
+        bool anythingAlright = true;
+        float tmpTerrainOffset;
+        double tmpGaussianVariance;
+
+        if (!float.TryParse(TerrainOffsetInput.text, out tmpTerrainOffset))
+        {
+            TerrainOffsetInput.image.color = Color.red;
+            anythingAlright = false;
+        }
+        if (!double.TryParse(GaussianVarianceInput.text, out tmpGaussianVariance))
+        {
+            GaussianVarianceInput.image.color = Color.red;
+            anythingAlright = false;
+        }
+
+        if (anythingAlright)
+        {
+            StaticClass.TerrainOffset = tmpTerrainOffset;
+            StaticClass.GaussianVariance = tmpGaussianVariance;
+            SceneManager.LoadScene("TerrainScene");
+        }
+    }
+}
---
+++ Assets/Scripts/StaticClass.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticClass
+{
+    public static float TerrainOffset;
+    public static double GaussianVariance;
+}
